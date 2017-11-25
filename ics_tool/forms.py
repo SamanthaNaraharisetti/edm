c49fec3c2511fdf7bc3f68267418a7dba393a3c2
SamanthaNaraharisetti <33463315+SamanthaNaraharisetti@users.noreply.github.com>
2017-11-25 07:25:03 -0700

    Update forms.py

--- ics_tool/forms.py
+++ ics_tool/forms.py
@@ -28,9 +28,42 @@ class AddDonorForm(forms.ModelForm):
         model = Donors
         fields = '__all__'
 
+class EditDonorForm(forms.ModelForm):
+    SearchQuery = forms.CharField(max_length=255)
+
+    class Meta:
+        model = Donors
+        fields = '__all__'
+        
+class AddDonationsForm(forms.ModelForm):
+    SelectDonor     = forms.CharField(max_length=100)
+    FirstName       = forms.CharField(max_length=100)
+    LastName        = forms.CharField(max_length=100)
+    DonorID         = forms.IntegerField()
+    OrganizationName= forms.CharField(max_length=100)
+    StreetAddress   = forms.CharField(max_length=100)
+    City            = forms.CharField(max_length=100)
+    State           = forms.CharField(max_length=100)
+    Zip             = forms.CharField(max_length=100)
+    PhoneNumber     = forms.CharField(max_length=15)
+    Email           = forms.EmailField(max_length=100)
+    Comments        = forms.CharField(max_length=100)
+    DonationDate    = forms.CharField(max_length=100)
+    TotalPounds     = forms.IntegerField()
+    PurchasedbyICS  = forms.BooleanField(max_length=2,required=False)
+    DonationComments= forms.CharField(max_length=100)
+    Category        = forms.CharField(max_length=100)
+    Pounds          = forms.IntegerField()
+    
+
+    class Meta:
+        model = Donors
+        fields = '__all__'
+        
 class SearchDonorForm(forms.ModelForm):
     SearchQuery = forms.CharField(max_length=255)
 
     class Meta:
         model = SearchDonor
         fields = '__all__'
+        
